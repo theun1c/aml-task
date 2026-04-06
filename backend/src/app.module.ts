ddcbd2b6ec740c12b53ac53850e9f38b69e098ac
theun1c <belovdima1122@yandex.ru>
2026-04-06 09:10:20 +0300

    fix: env load

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    AuthModule,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
