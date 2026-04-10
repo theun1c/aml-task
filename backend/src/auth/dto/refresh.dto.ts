fdefcf62b9a855a1b6f954b896f8b5cd8aa343d2
theun1c <belovdima1122@yandex.ru>
2026-04-11 01:39:30 +0300

    feat: add auth dtos

--- backend/src/auth/dto/refresh.dto.ts
+++ backend/src/auth/dto/refresh.dto.ts
@@ -1,6 +1,6 @@
 import { IsString } from 'class-validator';
 
-export class UserDto {
+export class RefreshDto {
   @IsString()
-  name!: string;
+  refreshToken: string;
 }
---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,12 @@
+import { IsString } from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  name: string;
+}
