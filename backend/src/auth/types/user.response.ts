ecc9d124c13615ae06c00e106c64a1bad20a2509
theun1c <belovdima1122@yandex.ru>
2026-04-11 01:45:49 +0300

    fix: upd id field type

--- backend/src/auth/types/user.response.ts
+++ backend/src/auth/types/user.response.ts
@@ -1,9 +1,8 @@
 import { IsString, IsUUID } from 'class-validator';
-import type { UUID } from 'crypto';
 
 export class UserResponse {
-  @IsUUID()
-  id: UUID;
+  @IsString()
+  id: string;
 
   @IsString()
   email: string;
