d4babe5064e2776513e72574cbcb29695fafd125
theun1c <belovdima1122@yandex.ru>
2026-04-11 04:57:51 +0300

    refactor: add new validators for login dto

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,9 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @IsString()
+  @IsEmail()
   email: string;
 
   @IsString()
+  @MinLength(8)
   password: string;
 }
