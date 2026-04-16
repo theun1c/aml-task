ab6ecdd1e45d95be5ba471c177dc5405c86f2ac4
theun1c <belovdima1122@yandex.ru>
2026-04-16 07:45:32 +0300

    fix: health controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
     }),
     AuthModule,
+    HealthController,
   ],
   providers: [PrismaService],
 })
